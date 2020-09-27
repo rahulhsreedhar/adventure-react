58b525675f1b281b5a84376b21e2aadfa5da17c4
Rahul H Sreedhar <rahul.h.1993@gmail.com>
2020-09-27 21:56:10 +0530

    route

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path='services' component={Services} />
+          <Route path='products' component={Products} />
+          <Route path='sign-up' component={SignUp} />
+          <Route path='./' exact component={Home} />
+
         </Switch>
       </Router>
     </>);
